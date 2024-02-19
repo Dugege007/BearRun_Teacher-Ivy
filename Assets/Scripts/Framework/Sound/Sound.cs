8215166e281ed36ff5b2713ef5037990dc0f0485
Dugege007 <351080175@qq.com>
2024-02-20 01:09:00 +0800

    207-创建音乐播放单例模式

---
+++ Assets/Scripts/Framework/Sound/Sound.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BearRun
+{
+    public class Sound : MonoSingleton<Sound>
+    {
+        public string ResourcesDir = "";
+
+        private AudioSource mBGM;
+        private AudioSource mSFX;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            mBGM = gameObject.AddComponent<AudioSource>();
+            mBGM.playOnAwake = false;
+            mBGM.loop = true;
+
+            mSFX = gameObject.AddComponent<AudioSource>();
+            mSFX.playOnAwake = false;
+            mSFX.loop = false;
+        }
+
+        // 播放背景音乐
+        public void PlayBGM(string audioName)
+        {
+            string oldName;
+            if (mBGM.clip == null)
+                oldName = "";
+            else
+                oldName = mBGM.clip.name;
+
+            if (oldName != audioName)
+            {
+                string path = ResourcesDir + "/" + audioName;
+                // 加载资源
+                AudioClip clip = Resources.Load<AudioClip>(path);
+                // 播放
+                if (clip != null)
+                {
+                    mBGM.clip = clip;
+                    mBGM.Play();
+                }
+            }
+        }
+
+        // 播放音效
+        public void PlaySFX(string audioName)
+        {
+            string path = ResourcesDir + "/" + audioName;
+            // 加载资源
+            AudioClip clip = Resources.Load<AudioClip>(path);
+
+            mSFX.PlayOneShot(clip);
+        }
+    }
+}
